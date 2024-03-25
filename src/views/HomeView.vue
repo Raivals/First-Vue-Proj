a6bc2e2ec60694a8d2ec7ce3f77cf3c6417e01b3
Romain <romain71.contact@gmail.com>
2024-03-25 10:47:13 +0100

    Add search input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
+<template>
+  <main class="container text-white">
+    <div class="relative pt-4 mb-8">
+      <input type="text" placeholder="Search for a city or State"
+        class="w-full px-1 py-2 bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+        </div>
+  </main>
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-  <main>
-    
-  </main>
-</template>
