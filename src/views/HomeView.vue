a6e4104dc3e27449fce6750694f973fb53fd68a7
Romain <romain71.contact@gmail.com>
2024-03-25 14:36:32 +0100

    Add city view to have the specified link in the search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,8 @@
           No results match your query, try a different term.
         </p>
         <template v-else>
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -21,7 +22,25 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+const previewCity = (searchResult) => {
+
+  const [city, state] = searchResult.place_name.split(", ");
+  router.push({
+    name: "cityView",
+    params: {
+      city: city,
+      state: state.replaceAll(" ", "")
+    },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey = "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
 const searchQuery = ref("");
