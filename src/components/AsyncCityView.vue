51c720933e18d4d0181c69c79b38bb6de8dc1a0d
Romain <romain71.contact@gmail.com>
2024-03-25 16:20:08 +0100

    add the calc codeline  for date time and hourly weather offset

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -12,7 +12,17 @@ const route = useRoute();
 const getWeatherData = async () => {
     try {
         const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=55eac4c8e1ea5be90ed532e3972ae179&units=imperial
-`);
+        `);
+        // calculate current date & time
+        const localOffset = new Date().getTimezoneOffset() * 60000;
+        const utc = weatherData.data.current.dt * 1000 + localOffset;
+        weatherData.data.currentTime =
+            utc + 1000 * weatherData.data.timezone_offset;
+
+        // calculate hourly weather offset
+        weatherData.data.hourly.forEach((hour) => {
+            const utc = hour.dt * 1000 + localOffset;
+        });
     } catch (error) {
         console.log(error)
     }
